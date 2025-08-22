afaedc3335bf861661ace25239ae873c83b55ec9
jh <jhbkr@MacBook-Air-de-jh.local>
2025-08-23 01:03:15 +0200

    fix: centrer correctement le spotlight sur les personnages
    - Correction de la position finale du spotlight (x: 50, y: 50)
    - Spotlight maintenant parfaitement centré sur les héros
    - Position basée sur les pourcentages de l'écran

--- components/intro-animation.tsx
+++ components/intro-animation.tsx
@@ -91,7 +91,7 @@ export default function IntroAnimation() {
 
     // Après quelques secondes, le projecteur "trouve" sa cible
     const targetTimer = setTimeout(() => {
-      setSpotlightPosition({ x: 0, y: 0 }) // Vrai centre de l'écran
+      setSpotlightPosition({ x: 50, y: 50 }) // Centre de l'écran (50% de la largeur et hauteur)
       setFoundTarget(true)
       clearInterval(interval)
 
